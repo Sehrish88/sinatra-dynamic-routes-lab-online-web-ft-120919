b97dbb9f31224e34a25aeaa68b15c1a3a387f02e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-21 20:22:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative 'config/environment'
 
 
-class App < Sinatra::Base	class App < Sinatra::Base
+class App < Sinatra::Base	
 	
   get "/reversename/:name" do
     @user_name = params[:name]
